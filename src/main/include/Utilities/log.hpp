11d62c98de3c53b3b3aa3f46edd09fb8c183a7e4
quentintyr <quentin.tyr@gmail.com>
2025-07-08 14:52:07 +0200

    ADD | simple logging file

---
+++ src/main/include/Utilities/log.hpp
@@ -0,0 +1,59 @@
+#ifndef _LOG_HG_
+#define _LOG_HG_
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <sstream>
+#include <ctime>
+#include <iomanip>
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define YELLOW  "\033[33m"
+#define GREEN   "\033[32m"
+#define CYAN    "\033[36m"
+#define BLUE    "\033[34m"
+#define PURPLE  "\033[35m"
+#define WHITE   "\033[37m"
+
+// type shi
+#define BOLD        "\033[1m"
+#define bold_res    "\033[22m"
+#define ITALICS     "\033[3m"
+#define UNDERLINE   "\033[4m"
+
+std::ofstream logFile;
+
+void SetupLogging() {
+    logFile.open("/home/pi/robot.log", std::ios::out | std::ios::app);
+    std::cout.rdbuf(logFile.rdbuf());
+    std::cerr.rdbuf(logFile.rdbuf());
+}
+    
+inline std::string current_time() {
+    using namespace std::chrono;
+    auto now = system_clock::now();
+    auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
+
+    std::time_t now_c = system_clock::to_time_t(now);
+    std::tm* now_tm = std::localtime(&now_c);
+
+    std::ostringstream oss;
+    oss << std::put_time(now_tm, "%Y-%m-%d %H:%M:%S");
+    oss << '.' << std::setfill('0') << std::setw(3) << ms.count();
+    return oss.str();
+}
+
+// default log infos
+#define LOG_INFO(value)    { std::cout << GREEN << "[INFO] " << RESET << value << "                                                  " << " [" << current_time() << "]" << std::endl; }
+#define LOG_WARN(value)    { std::cout << YELLOW << "[WARN] " << RESET << value << "                                                  " << " [" << current_time() << "]" << std::endl; }
+#define LOG_ERROR(value)   { std::cerr << RED << "[ERROR] " << RESET << value << "                                                  " << " [" << current_time() << "]" << std::endl; }
+
+// mode log infos
+#define LOG_AUTONOMOUS(value)   { std::cout << PURPLE << "[AUTONOMOUS] " << RESET << value << "                                                  " << " [" << current_time() << "]" << std::endl; }
+#define LOG_TELEOP(value)   { std::cout << CYAN << "[TELEOP] " << RESET << value << "                                                  " << " [" << current_time() << "]" << std::endl; }
+#define LOG_TEST(value)   { std::cout << YELLOW << "[TEST] " << RESET << value << "                                                  " << " [" << current_time() << "]" << std::endl; }
+#define LOG_DISABLED(value)   { std::cout << WHITE << "[CURRENT MODE] " << RESET << value << "                                                  " << " [" << current_time() << "]" << std::endl; }
+
+#endif
