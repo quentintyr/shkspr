465ab3f78c677a868ba3b17fe6e974dac25ddb55
quentintyr <quentin.tyr@gmail.com>
2025-07-09 15:59:16 +0200

    UPDATE | disabled init

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,15 +1,14 @@
 #include <fstream>
 #include "Robot.h"
-#include "AMCU.h"
+#include "subsystems/AMCU.h"
 #include "Constants.h"
 #include "Utilities/log.hpp"
 #include <networktables/NetworkTableInstance.h>
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc2/command/CommandScheduler.h>
 
-AMCU amcu;
-
-bool testWasEnabled = false; 
+bool testWasEnabled = false;
+bool firstTimeStartUP = false;
 
 void Robot::RobotInit() {
     amcu.initOmniDriveBase(kWheelRadius, kRobotRadius, kMotorLeft, kMotorRight, kMotorBack);
@@ -20,8 +19,14 @@ void Robot::RobotPeriodic() {
 }
 
 void Robot::DisabledInit() {
-    LOG_DISABLED(" Disabled");
-    testWasEnabled = false;
+    if (!firstTimeStartUP) {
+        firstTimeStartUP = true;
+        LOG_INFO("First Time Startup");
+    }
+    else {
+        LOG_DISABLED(" Disabled");
+        testWasEnabled = false;
+    }
 }
 
 void Robot::DisabledPeriodic() {}
@@ -30,13 +35,11 @@ void Robot::AutonomousInit() {
     testWasEnabled = false;
     LOG_AUTONOMOUS("Enabled");
     last_mode = {PURPLE, "[AUTONOMOUS]"};
-    //amcu.driveDistance(2, 0, 0); // Example: drive 2 meters in x
+    amcu.driveDistance(1, 0, 0); // Example: drive 2 meters in x
 }
 
 void Robot::AutonomousPeriodic() {
-    frc::SmartDashboard::PutNumber("Encoder Left", amcu.getEncoder(kMotorLeft));
-    frc::SmartDashboard::PutNumber("Encoder Right", amcu.getEncoder(kMotorRight));
-    frc::SmartDashboard::PutNumber("Encoder Back", amcu.getEncoder(kMotorBack));
+
 }
 
 void Robot::TeleopInit() {
