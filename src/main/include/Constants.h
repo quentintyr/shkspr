5079eb1eb6076aeefbdc45d04a875030c38065db
quentintyr <quentin.tyr@gmail.com>
2025-07-09 16:00:17 +0200

    ADD | map varaibles

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -4,8 +4,11 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
+#include <subsystems/AMCU.h>
 
 #pragma once
+
+extern AMCU amcu;
 
 // amcu omni drive base variables
 constexpr int kWheelRadius = 50;
@@ -13,6 +16,11 @@ constexpr int kRobotRadius = 150;
 constexpr Motor kMotorLeft  = MOTOR_1;
 constexpr Motor kMotorRight = MOTOR_3;
 constexpr Motor kMotorBack  = MOTOR_0;
+
+const int MAP_length = 400; //cm
+const int MAP_width = 200;
+
+
 
 /**
  * The Constants header provides a convenient place for teams to hold robot-wide
